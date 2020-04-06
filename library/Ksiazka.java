fe0e960c55965bc6f1408366462166734b7582b9
lesiuu125 <62214666+lesiuu125@users.noreply.github.com>
2020-04-06 21:50:50 +0200

    Update Ksiazka.java

--- library/Ksiazka.java
+++ library/Ksiazka.java
@@ -1,6 +1,5 @@
 public class Ksiazka implements Publikacja {
 
-    // pola
     String autor;
     String nazwa;
     int ilosc_stron;
@@ -16,27 +15,4 @@ public class Ksiazka implements Publikacja {
     }
 
 
-    // metody
-/*
-    public void wyslijSMS(int nr, String tekst) {
-        System.out.println("\n* Wysyłam SMS na nr: " + nr);
-        System.out.println("* O treści: " + tekst);
-    }
-
-    public String odbierzSMS() {
-        return "\n* SMS: Witaj telefonie!";
-    }
-
-    public void zadzwon(Telefon tel) {
-        System.out.println("\n* Ja, telefon marki " + marka + " o numerze " + numer);
-        System.out.println("* Dzwonię na nr: " + tel.numer);
-        // Wywołanie metody na obiekcie tel
-        tel.odbierzPolaczenie(numer);
-    }
-
-    public void odbierzPolaczenie(int nr) {
-        System.out.println("\n# Ja, telefon marki "+ marka + " o numerze " + numer);
-        System.out.println("# Odbieram połączenie od nr: " + nr);
-    }
-*/
 }
